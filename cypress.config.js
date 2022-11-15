f02c750ed4681fdaed962b6403ee33118144a3c9
trang.truong@gigacover.com <trang.truong@gigacover.com>
2022-11-15 16:22:07 +0700

    increase timeout

--- cypress.config.js
+++ cypress.config.js
@@ -3,7 +3,7 @@ const { defineConfig } = require("cypress");
 module.exports = defineConfig({
   projectId: "oqymk6",
   e2e: {
-    defaultCommandTimeout: 3000,
+    defaultCommandTimeout: 10000,
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
