3aaef476de89b8791781db9a25a551ae62f8aedd
trang.truong@gigacover.com <trang.truong@gigacover.com>
2022-11-18 11:36:33 +0700

    demo read and write file with cypress

--- cypress/e2e/readWriteFileDemo.cy.js
+++ cypress/e2e/readWriteFileDemo.cy.js
@@ -1,15 +1,35 @@
 
 
 before(function() {
+
     cy.log('this is before')
-    cy.fixture("example.json").as("test_data")
-    cy.log(test_data.name)
+    cy.fixture("example.json").as("test_data").then((test_data)=>{
+        cy.log(test_data.name)
+    })
+
 })
 
-// it("Demo read date from fixture", ()=> {
-//     cy.fixture('example.json').then((data)=> {
-//         cy.log(data.name)
-//     })
+it("Demo read data from fixture", function() {
+    cy.fixture('example.json').as('data').then((data)=> {
+        cy.log(data.email)
+    })
 
-    // cy.log(this.test_data.name)
-// })
+    cy.log(this.test_data.body)
+
+})
+
+it("Demo read file using readFile()", function(){
+    cy.readFile('./cypress/fixtures/example.json').then((data)=> {
+        cy.log(data.demo_readFile)
+    })
+})
+
+it("Demo write file", function(){
+    cy.writeFile('sample.txt', 'this is demo write file with cypress !!!\n')
+    cy.writeFile('sample.txt', 'write another line', {flag: 'a+'})
+
+})
+
+
+
+
